ed471b1699a657a446ca206a39884052da02473b
hasanisokay <hasanisokay@gmail.com>
2024-09-04 15:07:03 +0600

    added image onclick and registerd blot in begining

--- src/components/QuillRenderer.jsx
+++ src/components/QuillRenderer.jsx
@@ -1,34 +1,42 @@
 'use client'
 import { useEffect, useRef, useState } from 'react';
 import Quill from 'quill';
-import 'quill/dist/quill.core.css'; 
+import CustomImageBlot from './CustomImageBlot';
+import 'quill/dist/quill.core.css';
+import Image from 'next/image';
+
+// Register CustomImageBlot globally
+Quill.register(CustomImageBlot);
 
 const QuillRenderer = ({ content }) => {
   const containerRef = useRef(null);
   const [quill, setQuill] = useState(null);
-console.log(content)
+
   useEffect(() => {
     if (!content || !containerRef.current) return;
+
+    // Initialize Quill
     const quillInstance = new Quill(containerRef.current, {
       readOnly: true,
       theme: null,
     });
     setQuill(quillInstance);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [content]);
 
   useEffect(() => {
     if (!quill || !content) return;
+
+    // Set Quill content
     quill.setContents(content);
+
+    // Attach onClick to all images
     const images = containerRef.current.querySelectorAll('img');
     images.forEach((img) => {
       img.onclick = () => {
         window.open(img.src, '_blank');
       };
     });
-     }, [quill, content]);
-
-  
+  }, [quill, content]);
 
   return <div ref={containerRef}></div>;
 };
