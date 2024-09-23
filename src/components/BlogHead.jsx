c7bbe2c3a44f54da9e0b4fdb2e9e1572e3bbf7c0
hasanisokay <hasanisokay@gmail.com>
2024-09-23 23:15:11 +0600

    added categories fileter of empty category in blog head

--- src/components/BlogHead.jsx
+++ src/components/BlogHead.jsx
@@ -21,8 +21,8 @@ const BlogHead = ({ blog, clickable }) => {
                     <p className='flex gap-1 items-center'> <span>{calculateReadingTime(blog?.content)} min read</span> <Dot /> <span>{getTime(blog?.addedOn)}</span></p>
                 </div>
             </>}
-            <p className='text-sm'>Categories: {blog?.categories?.map((c, i) => <Link href={`/blogs/categories/${c}`} className="mr-1 text-blue-500 cursor-pointer" key={i}>{c}</Link>)}</p>
-        </>
+{blog?.categories.length >0 &&             <p className='text-sm'>Categories: {blog?.categories?.map((c, i) => <Link href={`/blogs/categories/${c}`} className="mr-1 text-blue-500 cursor-pointer" key={i}>{c}</Link>)}</p>
+}        </>
     );
 };
 
