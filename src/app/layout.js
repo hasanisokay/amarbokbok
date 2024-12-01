c85093ead6c206e12e54c1c8a033f4e8ffa1e7a6
hasanisokay <hasanisokay@gmail.com>
2024-12-01 21:35:28 +0600

    changed date in sitemap

--- src/app/layout.js
+++ src/app/layout.js
@@ -43,7 +43,7 @@ export default async function RootLayout({ children }) {
         </Providers>
         <Toaster />
       </body>
-      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+      {/* <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} /> */}
     </html>
   );
 }
