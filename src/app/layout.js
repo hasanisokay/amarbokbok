fd82124d026e3b4e576d70373b2c7ba8bf22b5e3
hasanisokay <hasanisokay@gmail.com>
2024-12-02 00:18:24 +0600

    commented 32x32 16x16 img

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default async function RootLayout({ children }) {
         <link rel="apple-touch-icon" sizes="180x180" href="./../../public/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="512x512" href="./../../public/android-chrome-512x512.png" />
         <link rel="icon" type="image/png" sizes="192x192" href="./../../public/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="./../../public/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="./../../public/favicon-16x16.png" />
+        {/* <link rel="icon" type="image/png" sizes="32x32" href="./../../public/favicon-32x32.png" /> */}
+        {/* <link rel="icon" type="image/png" sizes="16x16" href="./../../public/favicon-16x16.png" /> */}
       </head>
       <body className="transition-colors">
         <Providers initialTheme={storedTheme}>
